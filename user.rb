5b3f11ed8e958587dc5ec70c99cb70165c329049
kohey <kawasonblog@gmail.com>
2019-08-30 16:28:19 +0900

    リファクタリング

--- user.rb
+++ user.rb
@@ -1,20 +1,11 @@
-# ユーザログイン→位置情報の周りのmsgを取得
-# msgの位置情報→サーバたてっぱ
-# msgのpost→dbに保存
+
 require 'open-uri'
 require 'json'
 require 'net/http'
 
-# res = Net::HTTP.get_response(params)
-# json = JSON.parse(res.body)
-
-# p json
-
-# puts "hello"
 messages =  []
 
 File.open("db.json") do |file|
-  #保存されてるmessageの取得
   message_hash = JSON.load(file)
   users = Net::HTTP.get(URI.parse('http://localhost:3000/Users'))
   res = JSON.parse(users)
